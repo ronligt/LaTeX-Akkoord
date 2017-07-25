52aa723761e39930a49ddeb23ef1d71be66df542
Ronald Ligteringen <r.ligteringen@gmail.com>
2017-07-25 16:35:14 +0200

    clean up your stuff man...

---
+++ examples/pdf2png.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+convert -density 300 example_c7.pdf -quality 90 example_c7.png
+convert -density 300 example_c-7.pdf -quality 90 example_c-7.png
+convert -density 300 example_gmaj7.pdf -quality 90 example_gmaj7.png
+convert -density 300 example_d.pdf -quality 90 example_d.png
